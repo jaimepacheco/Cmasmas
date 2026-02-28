a521e67f6ecf5618fff686bbf227ec562f58da94
Jaime Pacheco <jaimepachecocristales@outlook.com>
2026-02-28 14:04:09 +0100

    second commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include<string>
+
 using namespace std;
 
+int area(int length, int width) {
+    return length * width;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    string text = "Hello, World!";
+    cout << text << endl;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Iteration: " << i << endl;
+    }
+
+    cout << "Area of rectangle with length 5 and width 3: " << area(5, 3) << endl;
+
     return 0;
+    
 }
